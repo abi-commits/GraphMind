13625c0bcb88e7bfdb053c80b44363c2f3ed6a22
abi-commits <abinesh3200@gmail.com>
2025-10-11 17:00:20 +0530

    Enhanced: UI

--- frontend/graphmind-ui/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:8000',
-  },
-}
-
-module.exports = nextConfig
