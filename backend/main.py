f978eb60216ddc6dd4e476ea3642290566b4c0fe
abi-commits <abinesh3200@gmail.com>
2025-10-31 12:04:39 +0530

    fix: python version

--- backend/main.py
+++ backend/main.py
@@ -31,8 +31,3 @@ def create_app() -> FastAPI:
     return app
 
 app = create_app()
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
